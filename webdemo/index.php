9880e17693c58733de24c880ff016c37e20c6335
SangRedComet <sang29631@gmail.com>
2022-10-27 09:18:14 +0700

    ahaha

--- webdemo/index.php
+++ webdemo/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-$urladmin ="http://localhost/shopp/admin/";
-$urluser ="http://localhost/shopp/";
+$urladmin ="http://postgres://oypqlcjyudheiy:13912114f57a7aaebb463f73218face5376961cfbc547f3bc124b024dc4411a9@ec2-44-199-9-102.compute-1.amazonaws.com:5432/dbha29cjdmnlfo/shopp/admin/";
+$urluser ="http://postgres://oypqlcjyudheiy:13912114f57a7aaebb463f73218face5376961cfbc547f3bc124b024dc4411a9@ec2-44-199-9-102.compute-1.amazonaws.com:5432/dbha29cjdmnlfo/shopp/";
 $home = "home.php";
 $register = "register.php";
 $shoppingcart = "shoppingcart.php";
@@ -9,10 +9,10 @@ $order = "order.php";
 $logout = "logout.php";
 
 //Connection
-$host = "localhost";
-$username="root";
-$password="";
-$db= "shopp";
-$conn = mysqli_connect($host,$username,$password,$db) or die("Can not connect database ".mysqli_connect_error());
+$host = "ec2-44-199-9-102.compute-1.amazonaws.com";
+$username="oypqlcjyudheiy";
+$password="13912114f57a7aaebb463f73218face5376961cfbc547f3bc124b024dc4411a9";
+$db= "dbha29cjdmnlfo";
+$conn = pg_connect(getenv("postgres://oypqlcjyudheiy:13912114f57a7aaebb463f73218face5376961cfbc547f3bc124b024dc4411a9@ec2-44-199-9-102.compute-1.amazonaws.com:5432/dbha29cjdmnlfo")) or die("Can not connect database ".mysqli_connect_error());
 
 include('theme.php');
