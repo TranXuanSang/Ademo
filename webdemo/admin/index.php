b8e54b379ba5735e2743120e31c5f7622dd46976
SangRedComet <sang29631@gmail.com>
2022-11-12 18:35:24 +0700

    Update index.php

--- webdemo/admin/index.php
+++ webdemo/admin/index.php
@@ -9,10 +9,13 @@ $order = "order.php";
 $logout = "logout.php";
 
 //Connection
+/*
 $host = "ec2-44-199-9-102.compute-1.amazonaws.com";
 $username="oypqlcjyudheiy";
 $password="13912114f57a7aaebb463f73218face5376961cfbc547f3bc124b024dc4411a9";
 $db= "dbha29cjdmnlfo";
 $conn = pg_connect(getenv("postgres://oypqlcjyudheiy:13912114f57a7aaebb463f73218face5376961cfbc547f3bc124b024dc4411a9@ec2-44-199-9-102.compute-1.amazonaws.com:5432/dbha29cjdmnlfo")) or die("Can not connect database ".mysqli_connect_error());
+*/
+$Connect = pg_connect("postgres://oypqlcjyudheiy:13912114f57a7aaebb463f73218face5376961cfbc547f3bc124b024dc4411a9@ec2-44-199-9-102.compute-1.amazonaws.com:5432/dbha29cjdmnlfo");
 
 include('theme.php');
