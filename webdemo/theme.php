16e9b4043774663b72835825693da7a7ff7906d8
SangRedComet <sang29631@gmail.com>
2022-11-12 20:02:55 +0700

    sa

--- webdemo/theme.php
+++ webdemo/theme.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ChisBout Store</title>
+    <title>demo store Store</title>
     <link rel="icon" type="image/x-icon" href="asset/Icon/adu.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
